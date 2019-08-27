ceaabd5a2b5106707742c20ce3f6a9936fbda1ab
Alessandro Rodi <alessandro.rodi@renuo.ch>
2019-08-27 09:44:26 +0200

    update email text

--- lib/redmine_communicator/email.ex
+++ lib/redmine_communicator/email.ex
@@ -13,7 +13,7 @@ defmodule RedmineCommunicator.Email do
         <> "In order to send correct invoices, all time entries must be associated to a project.\n"
         <> "Unfortunately, there are unhandled toggle-entries from your account. "
         <> "Please allocate them to a project by moving it to the correct tickets:\n"
-        <> "#{Application.get_env(:redmine_communicator, RedmineCommunicator.RedmineService)[:redmine_url]}/issues/916/time_entries\n" 
+        <> "#{Application.get_env(:redmine_communicator, RedmineCommunicator.RedmineService)[:redmine_url]}/projects/toggle-auto-time-entries/time_entries?issue_id=~916\n"
         <> "#{time_entries}\n"
         <> "You might find the right ticket here: #{Application.get_env(:redmine_communicator, RedmineCommunicator.RedmineService)[:redmine_url]}/projects/internal/wiki/Renuo_FAQ#Renuo-FAQ\n"
         <> "Or here #{Application.get_env(:redmine_communicator, RedmineCommunicator.RedmineService)[:dashboard_url]}\n"
